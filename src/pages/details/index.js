d52de1306dfebfe3bf501918a0b73eeaaa36ad25
李晓松 <lixiaosong59@163.com>
2019-05-29 14:55:52 +0800

    提交代码

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import router from 'umi/router';
 
 export default class HomePage extends React.Component{
 
     render (){
         return (
-            <div>
-                loading
+            <div onClick={()=>{router.push('/')}}>
+                details
             </div>
         )
     }
--- src/pages/homePage/index.js
+++ src/pages/homePage/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import router from 'umi/router';
 
 export default class HomePage extends React.Component{
 
     render (){
         return (
-            <div>
+            <div onClick={()=>{router.push('details')}}>
                 Lorem ipsum dolor.
             </div>
         )
